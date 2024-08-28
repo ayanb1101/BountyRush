ce6079e561b71b114b61eb275e8605bff330de23
Useless <43134820+ayanb1101@users.noreply.github.com>
2024-08-28 23:24:43 +0530

    Update bounty.ts

--- src/pages/api/bounty.ts
+++ src/pages/api/bounty.ts
@@ -153,7 +153,7 @@ function convertLastSixDigitsToZero(number:number) {
     // }
 
     // Convert the number to a string, take the first 4 digits, and append six zeros.
-    let result = Math.round((number/10000)*10000)
+    let result = Math.round(number/10000)*10000
 
     return result;
 }
